6155251803fca0703d6ef48e373e2e59760a89d1
Rob T. <techeducation@launchpadphilly.org>
2025-05-30 14:34:38 -0400

    updated tailwind.css

--- app/page.js
+++ app/page.js
@@ -2,7 +2,9 @@ import HeroSection from '@/components/HeroSection';
 import FeaturesSection from '@/components/FeaturesSection';
 import HowItWorksSection from '@/components/HowItWorksSection';
 import CTASection from '@/components/CTASection';
+
 import AnimatedBackground from '@/components/AnimatedBackground';
+import './globals.css';
 
 export default function Home() {
   return (
