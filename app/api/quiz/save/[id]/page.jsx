578ae48a1f2865c06e6deef1bc6b8b9b0eadb1ca
Rob T. <techeducation@launchpadphilly.org>
2025-07-24 22:20:42 -0400

    fix: Remove commented-out session retrieval to clean up code

--- app/api/quiz/save/[id]/page.jsx
+++ app/api/quiz/save/[id]/page.jsx
@@ -1,13 +1,11 @@
 import { prisma } from '../../../../../lib/prisma';
 import { getServerSession } from 'next-auth';
 import { NextResponse } from 'next/server';
-// import { authOptions } from '../../../auth/[...nextauth]/route';
 
 export async function GET(request, { params }) {
 
     try {
 
-        // const session = await getServerSession(authOptions);
 
         if (!session?.user) {
             return NextResponse.json(
